8f13d0d2e39a2562e95ed67d4bd6f071a4e6e7e1
sloik89 <sloik89@gmail.com>
2023-02-06 23:19:42 +0100

    adding structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React, { useState, useEffect } from "react";
+import Article from "./Article";
+import data from "./data";
 function App() {
-  return <div className="App">dziala</div>;
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overeacted</h1>
+          <button className="btn">toogle</button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Article.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Article = ({ title, snippet, date, length }) => {
+  return (
+    <article className="post">
+      <h2>{title}</h2>
+      <div className="post-info">
+        <span>date</span>
+        <span>{length}</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  );
+};
+
+export default Article;

f9ba1d5e48ccefb30c2c4cf18b2a9490e6a339d6
sloik89 <sloik89@gmail.com>
2023-02-07 00:04:50 +0100

    set the local storage and theme functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { useState, useEffect } from "react";
 import Article from "./Article";
 import data from "./data";
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+};
 function App() {
+  const [theme, setTheme] = useState(getStorageTheme());
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+  const handleTheme = () => {
+    setTheme((oldTheme) => {
+      let newTheme = oldTheme === "light-theme" ? "dark-theme" : "light-theme";
+      return newTheme;
+    });
+  };
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>overeacted</h1>
-          <button className="btn">toogle</button>
+          <button onClick={handleTheme} className="btn">
+            toogle
+          </button>
         </div>
       </nav>
       <section className="articles">
